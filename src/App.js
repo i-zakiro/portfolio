f2bfbf8d488107feccd5fa86bb7229accd9c9063
i-zakiro <zakiro24x7@gmail.com>
2023-03-12 17:41:15 +0600

    1619

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import "./App.css"
+import Header from "./components/header/Header";
 
 function App() {
   return (
-    <h1>Hellps</h1>
+    <>
+      <Header />
+    </>
   );
 }
 
