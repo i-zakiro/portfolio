453e881f65b4ceff1f15cdda48a4aa3af4cbf2dd
i-zakiro <zakiro24x7@gmail.com>
2023-03-16 21:03:40 +0600

    3726

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -49,10 +49,10 @@ const Header = () => {
                     </li>
                 </ul>
 
-                <i class="uil uil-times nav__close"></i>
+                <i class="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
             </div>
 
-            <div className="nav__toggle">
+            <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
                 <i class="uil uil-apps"></i>
             </div>
         </div>
