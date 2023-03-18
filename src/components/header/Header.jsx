2bad05e7c15c8b5a1f673fae5750f91b4ac11f38
i-zakiro <zakiro24x7@gmail.com>
2023-03-18 18:37:00 +0600

    4027

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,43 +8,43 @@ const Header = () => {
   return (
     <header className="header">
         <div className="nav container">
-            <a href="index.html" className="nav__logo">Smith</a>
+            <a href="index.html" className="nav__logo">Zakirov</a>
 
             <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
                 <ul className="nav__list grid">
                     <li className="nav__item">
                         <a href="#home" className="nav__link active-link">
-                            <i className="uil uil-estate nav__icon"></i> Home
+                            <i className="uil uil-estate nav__icon"></i> Главная
                         </a>
                     </li>
                     
                     <li className="nav__item">
                         <a href="#about" className="nav__link">
-                            <i className="uil uil-user nav__icon"></i> About
+                            <i className="uil uil-user nav__icon"></i> Обо мне
                         </a>
                     </li>
                     
                     <li className="nav__item">
                         <a href="#skills" className="nav__link">
-                            <i className="uil uil-file-alt nav__icon"></i> Skills
+                            <i className="uil uil-file-alt nav__icon"></i> Скилы
                         </a>
                     </li>
                     
                     <li className="nav__item">
                         <a href="#services" className="nav__link">
-                            <i className="uil uil-briefcase-alt nav__icon"></i> Services
+                            <i className="uil uil-briefcase-alt nav__icon"></i> Услуги
                         </a>
                     </li>
                     
                     <li className="nav__item">
                         <a href="#portfolio" className="nav__link">
-                            <i className="uil uil-scenery nav__icon"></i> Portfolio
+                            <i className="uil uil-scenery nav__icon"></i> Портфолио
                         </a>
                     </li>
                     
                     <li className="nav__item">
                         <a href="#contact" className="nav__link">
-                            <i className="uil uil-message nav__icon"></i> Contact
+                            <i className="uil uil-message nav__icon"></i> Контакты
                         </a>
                     </li>
                 </ul>
