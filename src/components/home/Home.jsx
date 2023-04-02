837ff24404daf383b06080cddcad509be3908da3
i-zakiro <90312517+i-zakiro@users.noreply.github.com>
2023-04-02 16:07:31 +0600

    5112

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import Data from './Data';
-import './home.css';
-import Social from './Social';
+import React from "react";
+import Data from "./Data";
+import "./home.css";
+import ScrollDown from "./ScrollDown";
+import Social from "./Social";
 
 const Home = () => {
   return (
     <section className="home section" id="home">
-        <div className="home__container container grid">
-            <div className="home__content grid">
-                <Social />
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <Social />
 
-                <div className="home__img"></div>
+          <div className="home__img"></div>
 
-                <Data />
-            </div>
+          <Data />
         </div>
-    </section>
-  )
-}
 
-export default Home
+        <ScrollDown />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
